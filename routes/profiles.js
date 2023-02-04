6b8a4a69c74141afc10332bee6aded9284eb118d
Mary Lueder <mlueder@gmail.com>
2023-02-04 12:26:07 -0500

    adjust profile router and controller to only fetch current users profile data

--- routes/profiles.js
+++ routes/profiles.js
@@ -9,7 +9,7 @@ const router = Router()
 
 /*---------- Protected Routes ----------*/
 router.use(decodeUserFromToken)
-router.get('/', checkAuth, profilesCtrl.index)
+router.get('/:id', checkAuth, profilesCtrl.index)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
 
 export { router }
